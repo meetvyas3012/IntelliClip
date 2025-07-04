4a3de0d50f366a43634ab841ffbe7c6f291ad0aa
Meet <meetvyas301204@gmail.com>
2025-07-04 14:01:55 +0530

    Initial commit

---
+++ backend/yt_download.py
@@ -0,0 +1,9 @@
+from pytubefix import YouTube
+from pytubefix.cli import on_progress
+url="https://www.youtube.com/watch?v=vPvnzWoK24Q"
+
+yt=YouTube(url,on_progress_callback=on_progress)
+print(yt.title)
+
+ys=yt.streams.get_highest_resolution()
+ys.download()
